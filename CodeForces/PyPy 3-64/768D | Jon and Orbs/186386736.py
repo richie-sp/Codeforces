6b02ba1d946194b7601b794ecae54aee61892a7b
Richie Pasumarty <124537234+richie-sp@users.noreply.github.com>
2023-02-05 21:29:37 -0600

    Committing files

---
+++ CodeForces/PyPy 3-64/768D | Jon and Orbs/186386736.py
@@ -0,0 +1,25 @@
+from sys import stdin
+input = stdin.readline
+ 
+k, q = map(int, input().split()); eps = 10 ** (-7)
+p_max = 0.5
+f = [[0.0 for i in range(k + 1)], [0.0 for i in range(k + 1)]]; f[0][0] = 1.0; f[1][1] = 1.0
+for i in range(2, 10 ** 5):
+    arr = [0.0 for i in range(k + 1)]
+    for j in range(1, k + 1):
+        arr[j] = f[-1][j] * j / k + f[-1][j - 1] * (k - j + 1) / k
+    f.append(arr)
+    if arr[k] >= p_max:
+        break
+ 
+L = [f[i][-1] for i in range(len(f))]
+ 
+for _ in range(q):
+    p = int(input());
+    for ind, elem in enumerate(L):
+        if p <= elem * 2000:
+            print(ind); break
+ 
+ 
+    
+    
