615356223de2e46aa4913e0499af86a110f590b4
Richie Pasumarty <124537234+richie-sp@users.noreply.github.com>
2023-02-05 21:28:31 -0600

    Committing files

---
+++ CodeForces/PyPy 3/1343D | Constant Palindrome Sum/189680557.py
@@ -0,0 +1,30 @@
+"""
+Accepted
+"""
+from sys import stdin
+from itertools import accumulate
+input = stdin.readline
+ 
+for case in range(int(input())):
+    n, k = map(int, input().split())
+    a = list(map(int, input().split()))
+ 
+    #have zero ops and 1 ops, can calculate 2 ops
+    zero_op_count = [0 for i in range(2*k + 2)]; psum_one_op = [0 for i in range(2*k + 2)]
+    for i in range(n // 2):
+        x = a[i]; y = a[n - 1 - i]
+        zero_op_count[x + y] += 1
+        minim_one = min(x, y) + 1; maxim_one = max(x, y) + k
+        psum_one_op[minim_one] += 1; psum_one_op[maxim_one + 1] -= 1
+        psum_one_op[x + y] -= 1; psum_one_op[x + y + 1] += 1
+ 
+    psum_one_op = list(accumulate(psum_one_op))
+    ans = float('inf')
+    for ind, one_ops in enumerate(psum_one_op):
+        two_ops = n // 2 - one_ops - zero_op_count[ind]
+        ops = two_ops * 2 + one_ops
+        ans = min(ans, ops)
+ 
+    print(ans)
+ 
+    
